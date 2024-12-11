6d18e34b3d131dbb3c1e13a8e7579971651890fd
supriya212003 <139315273+supriya212003@users.noreply.github.com>
2024-12-11 17:16:14 +0530

    js basic

---
+++ demo.js
@@ -0,0 +1,47 @@
+/*variables
+var a=10
+var b=14
+console.log(a+b)
+*/
+/*var a=10
+
+var b=a++//postincrement
+console.log(b)
+var b=++a//preincrement
+console.log(b)
+*/
+/*var a=10
+var b=a--//postdecrement
+console.log(b)
+var b=--a//predecrement
+console.log(b)
+*/
+/*
+var factor="Kamal"
+var fmovie="anbe sivam"
+var fplayer="dhoni"
+function favourite()//function declaration
+{
+    console.log("favourite actor is:"+factor)
+    console.log("favourite movie is:"+fmovie)
+    console.log("favourite player is:"+fplayer)
+}
+favourite()//function call
+*/
+/*
+function area(length,breadth)//function parameters
+{
+    console.log("area of rectangle is:"+length*breadth)
+}
+area(10,20)
+*/
+/*return type
+function add(a,b)
+{
+    return(a+b)
+
+}
+var total=add(10,20)
+console.log(toatal)
+*/
+
